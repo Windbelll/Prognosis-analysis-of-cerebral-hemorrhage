fc2c4111e0e0b4c93a49aa30bcc437c9788a1e6f
windbell <498045795@qq.com>
2022-12-09 08:35:00 +0800

    2022.12.9 version1.0

--- net/net_apis.py
+++ net/net_apis.py
@@ -21,11 +21,12 @@ def run(net, device, epochs, train_loader, val_loader, optimizer, loss_func, tra
     This is the function of training module
     """
     train_summary = SummaryWriter(log_dir="./logs/train" + current_time)
-    val_summary = SummaryWriter(log_dir="./logs/val" + current_time)
-    valSingle_summary = SummaryWriter(log_dir="./logs/val_s" + current_time)
+
     log = open("./logs/local" + current_time + ".txt", 'w')
     best_accuracy = 0.0
     best_epoch = 0
+    best_acc_batch = 0.0
+    best_epoch_batch = 0
     net = net.to(device)
     print("start training")
     for epoch in range(epochs):
@@ -50,29 +51,34 @@ def run(net, device, epochs, train_loader, val_loader, optimizer, loss_func, tra
             optimizer.step()
             train_loss += loss.item()
             # record images, loss
-            grid = torchvision.utils.make_grid(imgs)
-            train_summary.add_image("img", grid)
+            # grid = torchvision.utils.make_grid(imgs)
+            # train_summary.add_image("img", grid)
 
         val_accuracy, val_loss = val_without_info(net, val_loader, device, loss_func, val_c)
         vals_accuracy, _ = val_batch(net, device, loss_func)
         train_loss = train_loss / train_count
 
         train_summary.add_scalar("train_loss", train_loss, epoch)
-        val_summary.add_scalar("val_acc", val_accuracy, epoch)
-        valSingle_summary.add_scalar("val_s_acc", vals_accuracy, epoch)
+        train_summary.add_scalar("val_acc", val_accuracy, epoch)
+        train_summary.add_scalar("val_s_acc", vals_accuracy, epoch)
 
         if val_accuracy > best_accuracy:
             best_accuracy = val_accuracy
             best_epoch = epoch
-            torch.save(net, "./logs/best.pth")
-
+            torch.save(net, "./logs/best_every.pth")
+        if vals_accuracy > best_acc_batch:
+            best_acc_batch = vals_accuracy
+            best_epoch_batch = epoch
+            torch.save(net, "./logs/best_batch.pth")
         log_info = "-------\n" + "Epoch %d:\n" % (epoch + 1) + "Training" + " Loss: %.4f\n" % train_loss \
                    + "Val Accuracy: %4.2f%% @every || " % (val_accuracy * 100) + \
                    "%4.2f%% @batch" % (vals_accuracy * 100) + \
                    " || Loss: %.4f\n" % val_loss + \
                    "using time: %4.2f s\n" % (time.time() - start_time) + \
-                   "best acc: %4.2f%% @every" % (best_accuracy * 100) \
-                   + " produced @epoch %3d\n" % (best_epoch + 1)
+                   "best every acc: %4.2f%%" % (best_accuracy * 100) \
+                   + " produced @epoch %3d\n" % (best_epoch + 1) + \
+                   "best batch acc: %4.2f%%" % (best_acc_batch * 100) \
+                   + " produced @epoch %3d\n" % (best_epoch_batch + 1)
         log.write(log_info)
         print(log_info)
     return net
